4c7e85a1d8f87e563b9f1dfc7a9e65909b4381b9
agil20 <100156146+agil20@users.noreply.github.com>
2022-08-25 04:15:19 -0700

    feat:"X unit elave eledim amma islede bilmedim)

---
+++ ApiAplication/TestApilication/Controller/ProductControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using ApiAplication.Data;
+using AutoMapper;
+using FakeItEasy;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestApilication.Controller
+{
+   public  class ProductControllerTest
+    {
+
+        private readonly AppDbContext _context;
+        public static IWebHostEnvironment _env;
+        private readonly IMapper _mapper;
+
+        public ProductControllerTest()
+        {
+            _context = A.Fake<AppDbContext>();
+            _env = A.Fake<IWebHostEnvironment>();
+            _mapper = A.Fake<IMapper>();
+        }
+    }
+}
